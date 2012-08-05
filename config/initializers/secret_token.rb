6e47d89e60d7528d41710881cb7ec9b0e5e83766
lmduc <leminhducktvn@gmail.com>
2012-08-05 16:49:13 +0700

    [f] First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryJs::Application.config.secret_token = '09b1a0676dea4c9894bcd049132aac5f78623eaa33bdf17cabbb4fa126c8fd70890a8e6ca0b95c110cfa33929283052e6094b5da79ace8f53eae956cfaeb02ab'
